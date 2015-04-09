a9027ff148c2062133a52e2079e5d4e822490d2b
Eric Yang <windfarer@gmail.com>
2015-04-09 16:40:33 +0800

    adding copy static files

--- __init__.py
+++ __init__.py
@@ -4,14 +4,32 @@ import argparse
 import os
 import re
 import http.server
+import shutil
 
-env = Environment(loader=PackageLoader(__name__, 'templates'))
+TEMPLATE_FOLDER = "templates"
+SITE_FOLDER = "site"
+static_folders = ["css", "js"]
 
-template = env.get_template("default.html")
-md = open('posts/test.md', 'r').read()
-content = markdown.markdown(md)
-f = open('site/test.html', 'w')
 
-f.write(template.render(say=content))
+def create_site():
+    env = Environment(loader=PackageLoader(__name__, 'templates'))
 
-f.close()
+    template = env.get_template("default.html")
+
+    md = open('posts/test.md', 'r').read()
+    content = markdown.markdown(md)
+
+    f = open('site/test.html', 'w')
+    f.write(template.render(say=content))
+    f.close()
+
+    for dir in static_folders:
+        tpl_dir = os.path.join(TEMPLATE_FOLDER, dir)
+        site_dir = os.path.join(SITE_FOLDER, dir)
+        if os.path.exists(tpl_dir):
+            if os.path.exists(site_dir):
+                shutil.rmtree(site_dir)
+            shutil.copytree(tpl_dir, site_dir)
+
+if __name__ == "__main__":
+    create_site()
