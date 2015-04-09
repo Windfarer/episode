5984d6665984f29c52a1bf3a4fa0adacbdfe175e
Eric Yang <windfarer@gmail.com>
2015-04-09 18:25:02 +0800

    change structure

--- episode.py
+++ episode.py
@@ -1,4 +1,4 @@
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
 import markdown
 import argparse
 import os
@@ -6,26 +6,27 @@ import re
 import http.server
 import shutil
 
+PROJECT_FOLDER = "project"
 TEMPLATE_FOLDER = "templates"
 SITE_FOLDER = "site"
 static_folders = ["css", "js"]
 
 
 def create_site():
-    env = Environment(loader=PackageLoader(__name__, 'templates'))
+    env = Environment(loader=FileSystemLoader("project/templates"))
 
     template = env.get_template("index.html")
 
-    md = open('posts/index.md', 'r').read()
+    md = open('project/posts/index.md', 'r').read()
     content = markdown.markdown(md)
 
-    f = open('site/index.html', 'w')
+    f = open('project/site/index.html', 'w')
     f.write(template.render(content=content))
     f.close()
 
     for dir in static_folders:
-        tpl_dir = os.path.join(TEMPLATE_FOLDER, dir)
-        site_dir = os.path.join(SITE_FOLDER, dir)
+        tpl_dir = os.path.join(PROJECT_FOLDER, TEMPLATE_FOLDER, dir)
+        site_dir = os.path.join(PROJECT_FOLDER, SITE_FOLDER, dir)
         if os.path.exists(tpl_dir):
             if os.path.exists(site_dir):
                 shutil.rmtree(site_dir)
