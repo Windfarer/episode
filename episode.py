823c865157a356fc2159a8b4c5f00dff93ca8dfb
Eric Yang <windfarer@gmail.com>
2015-04-10 14:25:35 +0800

    adding root path md file generation.

--- episode.py
+++ episode.py
@@ -8,10 +8,11 @@ import shutil
 
 PROJECT_FOLDER = "project"
 TEMPLATE_FOLDER = "templates"
-POST_FOLDER = ""
+POST_FOLDER = "posts"
 SITE_FOLDER = "site"
 static_folders = ["css", "js"]
 EXT_LIST = [".md", ".markdown"]
+
 
 def create_site():
     env = Environment(loader=FileSystemLoader("project/templates"))
@@ -38,27 +39,37 @@ def create_site():
 def walk_md():
     env = Environment(loader=FileSystemLoader("project/templates"))
     template = env.get_template("index.html")
-
     file_name_pattern = re.compile(r"(?P<year>(\d{4}))\-(?P<month>(\d{1,2}))\-(?P<day>(\d{1,2}))\-(?P<title>(.+))")
-    for root, dirs, files in os.walk(os.path.join(PROJECT_FOLDER, POST_FOLDER)):
-        for file in files:
-            file_name, file_ext = os.path.splitext(file)
-            if file_ext in EXT_LIST:
-                p = re.match(file_name_pattern, file_name)
-                year = p.group("year")
-                month = p.group("month")
-                day = p.group("day")
-                title = p.group("title")
-                md = open(os.path.join(root,file), 'r').read()
-                content = markdown.markdown(md)
-                render_folder = os.path.join(PROJECT_FOLDER, SITE_FOLDER, year, month, day)
-                if not os.path.exists(render_folder):
-                    os.makedirs(render_folder)
-                f = open(os.path.join(render_folder, title+".html"), 'w')
-                f.write(template.render(content=content))
-                f.close()
+    post_dir = os.path.join(PROJECT_FOLDER, POST_FOLDER)
+    for file in os.listdir(post_dir):
+        file_name, file_ext = os.path.splitext(file)
+        if file_ext in EXT_LIST:
+            p = re.match(file_name_pattern, file_name)
+            year = p.group("year")
+            month = "{:02}".format(int(p.group("month")))
+            day = "{:02}".format(int(p.group("day")))
+            title = p.group("title")
+            print(year, month, day)
+            md = open(os.path.join(post_dir, file), 'r').read()
+            content = markdown.markdown(md)
+            render_folder = os.path.join(PROJECT_FOLDER, SITE_FOLDER, year, month, day)
+            if not os.path.exists(render_folder):
+                os.makedirs(render_folder)
+            f = open(os.path.join(render_folder, title+".html"), 'w')
+            f.write(template.render(content=content))
+            f.close()
 
-
+    for file in os.listdir(PROJECT_FOLDER):
+        file_name, file_ext = os.path.splitext(file)
+        if file_ext in EXT_LIST:
+            md = open(os.path.join(PROJECT_FOLDER, file), 'r').read()
+            content = markdown.markdown(md)
+            render_folder = os.path.join(PROJECT_FOLDER, SITE_FOLDER)
+            if not os.path.exists(render_folder):
+                os.makedirs(render_folder)
+            f = open(os.path.join(render_folder, file_name+".html"), 'w')
+            f.write(template.render(content=content))
+            f.close()
 
 if __name__ == "__main__":
     walk_md()
---
+++ project/index.md
@@ -0,0 +1,7 @@
+---
+title: index
+template: index
+---
+# helllo
+## world
+
