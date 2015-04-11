3e244770c2f64b525d284ab56291fcf880f158e7
Eric Yang <windfarer@gmail.com>
2015-04-11 20:18:35 +0800

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='episode',
+    version='0.0.1',
+    packages=[''],
+    url='https://www.github.com/windfarer/episode',
+    license='MIT',
+    author='Windfarer',
+    author_email='windfarer@gmail.com',
+    description='A simple page generator.'
+)
